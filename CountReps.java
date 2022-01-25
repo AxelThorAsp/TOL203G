86922d2f289b545c82fa2bcc82c4c2f8b2819ef4
AxelThorAsp <axel.aspelund@gmail.com>
2022-01-25 14:49:52 +0000

    vika 2

---
+++ CountReps.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
+
+public class CountReps {
+  public static void main(String[] args) {
+    int[] a = StdIn.readAllInts();
+    Stopwatch s = new Stopwatch();
+    int rep = countReps(a);
+    double t = s.elapsedTime();
+    StdOut.println(rep);
+    StdOut.println(t);
+
+  }
+  public static int countReps(int[] a) {
+    return 0;
+  }
+}
