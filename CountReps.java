a00d955b61c71a8eaa040cc5c6b226f24cc748ca
AxelThorAsp <axel.aspelund@gmail.com>
2022-01-26 16:06:47 +0000

    new

--- CountReps.java
+++ CountReps.java
@@ -7,11 +7,34 @@ public class CountReps {
     Stopwatch s = new Stopwatch();
     int rep = countReps(a);
     double t = s.elapsedTime();
-    StdOut.println(rep);
+    StdOut.println("pairs: "+rep);
     StdOut.println(t);
 
   }
   public static int countReps(int[] a) {
-    return 0;
+    int count=0;
+    int pairs=0;
+    Arrays.sort(a);
+    for (int i =0 ;i<a.length-1 ;i++ ) {
+        if (a.length==1 || a.length==0) {
+          return 0;}
+
+        if (a[i]==a[i+1]) {count++;}
+        else {
+          int s=1;
+          for (int j=1;j<=2;j++ ) {
+            s=s*(count+1-(2-j))/j;
+        }
+        pairs=pairs+s;
+        count=0;}
+        if (i+2==a.length) {
+          int s=1;
+          for (int j=1;j<=2;j++ ) {
+            s=s*(count+1-(2-j))/j;
+          }
+          pairs=pairs+s;
+        }
+    }
+
+    return pairs;}
   }
-}
