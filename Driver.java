544a6e3d9eab104a14e996208b7659f63b0aa5f3
AxelThorAsp <axel.aspelund@gmail.com>
2022-02-02 14:42:51 +0000

    vika 3

---
+++ Driver.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
+
+public class Driver{
+
+  public static void main(String[] args) {
+    QuickFindUF uf = new QuickFindUF(10);
+     while (!StdIn.isEmpty()) {
+       int p = StdIn.readInt();
+       int q = StdIn.readInt();
+       if (!uf.connected(p,q)){
+         uf.union(p,q);
+         StdOut.print("Now connected: " + p + " ");
+         StdOut.print(q +"\n" );
+       }
+     }
+    StdOut.println(Arrays.toString(uf.getId()));
+   }
+ }
