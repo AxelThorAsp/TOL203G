7b96718e6429fb32f9f9d2174e7c9007ccd88bfb
AxelThorAsp <axel.aspelund@gmail.com>
2022-02-09 16:04:48 +0000

    LLnode

---
+++ LLNode.java
@@ -0,0 +1,19 @@
+public class LLNode {
+  public String item;
+  public LLNode next;
+
+
+  public static void main(String[] args) {
+    LLNode ret = new LLNode();
+    LLNode last = ret;
+    LLNode next = new LLNode();
+
+    last.next=next;
+
+    last.item="A";
+    last = last.next;
+    last.item="B";
+    LLMerge.print(ret);
+    LLMerge.print(last);
+  }
+}
