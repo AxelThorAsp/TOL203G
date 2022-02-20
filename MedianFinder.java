f3e358c0a9d7c8eac6c1ea5c2478ec5a6bd539db
AxelThorAsp <axel.aspelund@gmail.com>
2022-02-20 17:24:13 +0000

    vika 6

---
+++ MedianFinder.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.*;
+
+public class MedianFinder<Key extends Comparable<Key>> {
+
+  public MedianFinder() {
+
+  }
+
+  public Key findMedian() {
+    return null;
+  }
+
+  public Key deleteMedian() {
+    return null;
+  }
+
+  public void insert(Key key) {
+
+  }
+
+  public static void main(String[] args) {
+    MedianFinder<String> m = new MedianFinder<String>();
+
+    m.insert("A");
+    m.insert("A");
+    m.insert("A");
+
+    StdOut.println(m.findMedian());
+  }
+
+
+}
