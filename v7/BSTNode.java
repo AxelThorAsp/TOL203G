de04d616fa827d670c491b5d0239ca5362662ac1
AxelThorAsp <axel.aspelund@gmail.com>
2022-03-01 12:09:07 +0000

    vika 7

---
+++ v7/BSTNode.java
@@ -0,0 +1,10 @@
+public class BSTNode{
+	public String key;
+	public BSTNode left, right;
+
+
+	public BSTNode(String key){
+		this.key = key; 
+
+	}
+}
---
+++ v7/BSTUtil.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.*;
+public class BSTUtil{
+	public static BSTNode min(BSTNode x){
+		//code
+	}
+
+	public static BSTNode secondMin(BSTNode x){
+		//code
+	}
+}
