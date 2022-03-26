884434e19e3b4752d10e13c4748558c7225c1ac7
AxelThorAsp <axel.aspelund@gmail.com>
2022-03-26 13:38:26 +0000

    vika 11

---
+++ v11/GraphArithmetic.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.*;
+public class GraphArithmetic{
+
+
+    //daemi
+    //inn: calculate(dg,vv);
+
+    //thar sem:
+    //String[] values = {"+","3","2","1"}; 
+    //Digraph dg = new Digraph(4);
+    //dg.addEdge(0,1);
+    //dg.addEdge(0,2);
+    //dg.addEdge(0,3);
+
+    //ut: 6.0
+
+    public static double calculate(Digraph dg, String[] vertexValues){
+        
+
+        return 0.0;
+    }
+
+    
+
+}
---
+++ v11/graph_2_9.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.*;
+public class graph_2_9{
+//Write a method that checks whether or not a given permutation of a DAG’s vertices 
+//is a topological order of that DAG.
+    public static boolean checkOrder(Digraph dg, Integer[] topologicalOrder){
+        return true;
+    }
+    
+}
